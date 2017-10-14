a3e0c9b86555c68e5fcda1a6c0a1f9c2a396ed7b
Dragomir Ivanov <dragomir.kr.ivanov@gmail.com>
2017-10-14 10:17:29 +0300

    Initial project

---
+++ Upr/14.10.17/Hello World/Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Dragomir Ivanov on 14.10.17.
+//  Copyright © 2017 SwiftFMI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

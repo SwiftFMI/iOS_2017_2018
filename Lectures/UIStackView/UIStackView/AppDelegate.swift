6e797f5dd7102ec62aa4c2f28acf4b0ad5abdcd2
Dragomir Ivanov <dragomir.ivanov@trading212.com>
2017-11-10 16:50:43 +0200

    UIStackView

---
+++ Lectures/UIStackView/UIStackView/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  UIStackView
+//
+//  Created by Dragomir Ivanov on 10.11.17.
+//  Copyright © 2017 SwiftFMI. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool { return true }
+
+}
+

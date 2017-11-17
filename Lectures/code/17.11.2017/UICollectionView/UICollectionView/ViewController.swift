a81a2897ac47744e934460ec6ab214efc7150562
Emil Atanasov <heitara@gmail.com>
2017-11-17 11:05:40 +0200

    добавен е UICollectionDataSource

--- Lectures/code/17.11.2017/UICollectionView/UICollectionView/ViewController.swift
+++ Lectures/code/17.11.2017/UICollectionView/UICollectionView/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource {
 
+    @IBOutlet weak var viewCollection: UICollectionView!
+    var items:[String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //add mock data
+        for i in 1 ... 5 {
+            items.append("Item #\(i)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // :MARK UICollectionViewDataSource methods
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellTypeOne", for: indexPath)
+        
+        return cell
+    }
 }
 

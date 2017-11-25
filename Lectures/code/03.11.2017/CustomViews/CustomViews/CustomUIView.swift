2a299f81835ac6d2d66febbda65b8b55fdaa0999
Emil Atanasov <heitara@gmail.com>
2017-11-25 12:47:17 +0200

    Празно къстъм-view

--- Lectures/code/03.11.2017/CustomViews/CustomViews/CustomUIView.swift
+++ Lectures/code/03.11.2017/CustomViews/CustomViews/CustomUIView.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2017 SwiftFMI. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class CustomUIView: UIView {
+    //used when initialised through code
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setup()
+    }
+    //used when initialised through storyboard 
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup() {
+        print("\(#function)")
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        //TODO: do the custom drawing
+    }
+}

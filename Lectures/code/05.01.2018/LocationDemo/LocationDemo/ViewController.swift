2fb3b9f45120c67cc62c7461d1a3cc88fb01e91b
Emil Atanasov <heitara@gmail.com>
2018-01-05 14:25:43 +0200

    add device required capabilities

--- Lectures/code/05.01.2018/LocationDemo/LocationDemo/ViewController.swift
+++ Lectures/code/05.01.2018/LocationDemo/LocationDemo/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    //UIRequiredDeviceCapabilities в Info.plist се използва за да се определи дали дадено устройство има съответния хардуер и дали приложението ще работи location-services или gps
+    
+    //Ако приложението може да работи и без тези изисквания, тогава не е нужно да добавяте
+    //тези изсквания, понеже броя на поддържаните устройства ще е по-малък.
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

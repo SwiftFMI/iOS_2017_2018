3e7161f077c5cf51339d79ae1754b4e4b0821e96
Dragomir Ivanov <dragomir.kr.ivanov@gmail.com>
2018-01-06 10:19:11 +0200

    photo library demo initial

---
+++ Upr/06.01.2018/PhotoLibraryDemo/PhotoLibraryDemo/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  PhotoLibraryDemo
+//
+//  Created by Dragomir Ivanov on 6.01.18.
+//  Copyright © 2018 Swift FMI. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+}
